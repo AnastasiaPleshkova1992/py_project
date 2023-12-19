6f7ed12b12c591e927282b52e3336cbf3286c175
Anastasia Pleshkova <nastya.tsvetkova@rambler.ru>
2023-12-19 15:40:49 -0500

    add_new_function

--- funcs.py
+++ funcs.py
@@ -3,3 +3,9 @@ def add_nums():
     num2 = int(input("second number: "))
     sum = num1 + num2
     return sum
+
+def subtract_nums():
+    num1 = int(input("first number: "))
+    num2 = int(input("second number: "))
+    difference = num1 - num2
+    return difference
